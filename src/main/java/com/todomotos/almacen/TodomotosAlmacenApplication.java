5658c705c55ae51adeab0e3598e562160f6a2765
Sebastian Quintero <squinte@bancolombia.com>
2021-08-01 12:05:54 -0500

    creacion del ambiente del proyecto con IntelliJ

---
+++ src/main/java/com/todomotos/almacen/TodomotosAlmacenApplication.java
@@ -0,0 +1,13 @@
+package com.todomotos.almacen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodomotosAlmacenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodomotosAlmacenApplication.class, args);
+	}
+
+}
