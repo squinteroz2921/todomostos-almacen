2fde53c2a81cf6e82172251265882b140fb6c3d6
Sebastian Quintero <squinteroz2921@gmail.com>
2021-08-18 15:46:48 -0500

    Se crean los mappers para PurchaseItem y Purchase

---
+++ src/main/java/com/todomotos/almacen/domain/repository/PruchaseRepository.java
@@ -0,0 +1,14 @@
+package com.todomotos.almacen.domain.repository;
+
+import com.todomotos.almacen.domain.Purchase;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface PruchaseRepository {
+    List<Purchase> getAll();
+    Optional<List<Purchase>> getByClient(String clientId);
+    Purchase save (Purchase purchase);
+}
