64d66bbb1d4dbfafff66249baf592589c684a466
Sebastian Quintero <squinteroz2921@gmail.com>
2021-08-17 20:34:41 -0500

    pruebas de inyeccion de dependencias

--- src/main/java/com/todomotos/almacen/web/controller/ProductController.java
+++ src/main/java/com/todomotos/almacen/web/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.todomotos.almacen.web.controller;
 import com.todomotos.almacen.domain.Product;
 import com.todomotos.almacen.domain.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,13 +11,23 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/products")
 public class ProductController {
+    @RequestMapping("/products")
+    public String home(){
+        return "Hola macaco";
+    }
+
     @Autowired
     private ProductService productService;
+
+
+    @GetMapping("/all")
     public List <Product> getAll(){
-        return productService.getAll();
-    }
+
+      return productService.getAll(); }
+
+
+
     public Optional <List<Product>> getByCategoryId(int categoryId){
         return productService.getByCategory(categoryId);
     }
